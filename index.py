7fb122682de3d03a3d6a1271759b6c37b4f76b68
felipe060 <felipeflip29@hotmail.com>
2024-06-03 12:20:21 -0300

    15º -->

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
 
 
-def send_email_200():
+"""def send_email_200():
     email_body = 'website status --> Response [200]'
     print('email_body --> ', email_body)
     print('email_body type -->', type(email_body))
@@ -58,9 +58,60 @@ while True:
     print(requisicao)
 
     if requisicao == '<Response [200]>':
-        send_email_200(
+        send_email_200()
 
     else:
-        send_email_error(
+        send_email_error()
 
-    time.sleep(20)
+    time.sleep(20)"""
+
+
+def send_email():
+    while True:
+        time.sleep(60)
+        current_time = datetime.datetime.now()
+        print(current_time)
+        requisicao = requests.get('https://flasklogin.vercel.app')
+        requisicao = str(requisicao)
+        print(requisicao)
+
+        if requisicao == '<Response [200]>':
+            email_body = 'website status --> Response [200]'
+            print('email_body --> ', email_body)
+            print('email_body type -->', type(email_body))
+
+            msg = email.message.Message()
+            msg['Subject'] = 'Python flask login vercel'
+            msg['From'] = 'felipica7@gmail.com'
+            msg['To'] = 'felipica7@gmail.com'
+            password = environ.get('PASSWORD_GOOGLE')
+            msg.add_header('Content-Type', 'text/html')
+            msg.set_payload(email_body)
+
+            s = smtplib.SMTP('smtp.gmail.com: 587')
+            s.starttls()
+            s.login(msg['From'], password)
+            s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+            print('email sent, I hope')
+
+        else:
+            email_body = 'website status --> deu errado alguma coisa aí, meu nobre'
+            print('email_body --> ', email_body)
+            print('email_body type -->', type(email_body))
+
+            msg = email.message.Message()
+            msg['Subject'] = 'Python flask login vercel'
+            msg['From'] = 'felipica7@gmail.com'
+            msg['To'] = 'felipica7@gmail.com'
+            password = environ.get('PASSWORD_GOOGLE')
+            msg.add_header('Content-Type', 'text/html')
+            msg.set_payload(email_body)
+
+            s = smtplib.SMTP('smtp.gmail.com: 587')
+            s.starttls()
+            s.login(msg['From'], password)
+            s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+            print('email sent, I hope')
+
+
+send_email()
