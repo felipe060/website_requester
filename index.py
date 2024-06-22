2b066a4228aa2ba8c20c88cad2d94be0125838da
felipe060 <felipeflip29@hotmail.com>
2024-06-22 10:52:59 -0300

    32º --> safe point

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
 
 
-"""def send_email_200():
+def send_email_200():
     email_body = 'website status --> Response [200]'
     print('email_body --> ', email_body)
     print('email_body type -->', type(email_body))
@@ -46,50 +46,17 @@ def send_email_error():
     s.starttls()
     s.login(msg['From'], password)
     s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
-    print('email sent, I hope')"""
+    print('email sent, I hope')
 
 
 while True:
-    time.sleep(20)
     requisicao = requests.get('https://flasklogin.vercel.app')
     requisicao = str(requisicao)
     print(requisicao)
 
     if requisicao == '<Response [200]>':
-        email_body = 'website status --> Response [200]'
-        print('email_body --> ', email_body)
-        print('email_body type -->', type(email_body))
-
-        msg = email.message.Message()
-        msg['Subject'] = 'Python flask login vercel'
-        msg['From'] = 'felipica7@gmail.com'
-        msg['To'] = 'felipica7@gmail.com'
-        password = environ.get('PASSWORD_GOOGLE')
-        msg.add_header('Content-Type', 'text/html')
-        msg.set_payload(email_body)
-
-        s = smtplib.SMTP('smtp.gmail.com: 587')
-        s.starttls()
-        s.login(msg['From'], password)
-        s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
-        print('email sent, I hope')
-
+        send_email_200(     #u must close this parentesis, then the code will work, i hope
+        time.sleep(20)
     else:
-        email_body = 'website status --> deu errado alguma coisa aí, meu nobre'
-        print('email_body --> ', email_body)
-        print('email_body type -->', type(email_body))
-
-        msg = email.message.Message()
-        msg['Subject'] = 'Python flask login vercel'
-        msg['From'] = 'felipica7@gmail.com'
-        msg['To'] = 'felipica7@gmail.com'
-        password = environ.get('PASSWORD_GOOGLE')
-        msg.add_header('Content-Type', 'text/html')
-        msg.set_payload(email_body)
-
-        s = smtplib.SMTP('smtp.gmail.com: 587')
-        s.starttls()
-        s.login(msg['From'], password)
-        s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
-        print('email sent, I hope')
-
+        send_email_error()
+        time.sleep(20)
